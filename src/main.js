1470162a0c0d6c0221fa99248a2466b5e786897e
reyesvi92@gmail.com <reyesvi92@gmail.com>
2020-01-20 11:13:29 -0300

    validando correo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import {
+    myFunction
+} from './lib/index.js';
 
 myFunction();
+
+const validation = () => {
+    valorEmail = document.getElementById("mail").value;
+    if (!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor))) {
+        alert('[ERROR] El campo debe tener un valor de correo electrónico');
+        return false;
+    }
+    valorPassword = document.getElementById("password-example").value;
+    if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
+        alert('[ERROR] El campo debe tener un valor de 8 caracteres');
+        return false;
+    }
+    return true;
+}
+validation();
+
+const btnRegistro = document.getElementById("registro");
+btnRegistro.addEventListener("click", () => {
+    const formsNone = document.getElementById("forms");
+    formsNone.style.display = "none";
+    const pagesPerfil = document.getElementById("root-post");
+    pagesPerfil.style.display = "block";
+})
