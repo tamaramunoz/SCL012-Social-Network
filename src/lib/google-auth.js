f06228da83b900104fa81717e1d242b9be3cc173
reyesvi92@gmail.com <reyesvi92@gmail.com>
2020-01-22 14:02:31 -0300

    cambios login exist

--- src/lib/google-auth.js
+++ src/lib/google-auth.js
@@ -1,15 +1,7 @@
 // aqui exportaras las funciones que necesites
 // LOGIN CON GOOGLE
+const GoogleIn = document.getElementById("loginGoogle");
 export const googleLogin = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
-
-  // Using a redirect.
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token.
-    const token = result.credential.accessToken;
-  }
-  const user = result.user;
-});
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
 }
