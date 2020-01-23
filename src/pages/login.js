45d22637ac7fb591cb7bedaec2bca0dc7514557b
NPGonzalez <naypili@gmail.com>
2020-01-23 13:38:33 -0300

    Prueba

---
+++ src/pages/login.js
@@ -0,0 +1,80 @@
+import {
+  googleLogin
+} from '../lib/google-auth.js';
+
+import {
+  facebookLogin
+} from '../lib/facebook-auth.js';
+
+import {
+  emailLogin 
+} from '../lib/index.js';
+
+import {
+  createAccount
+} from '../lib/index.js';
+
+
+
+//GENERACIÓN DE PÁGINA DE LOGUEO CON FIREBASE
+export const goLoginPage = () => {
+
+  document.getElementById("root").innerHTML =
+    `<div id="form-login">
+        <form action="/form-page" method="post" onsubmit="return validation()">
+          <ul class= "list">
+            <li>
+              <label for="mail"></label>
+              <input class="inputLogin" type="email" id="txtMail" name="user_mail" Placeholder = "Correo electrónico" />
+            </li>
+            <li>
+              <label for="password-example"></label> 
+              <input class="inputLogin" type="password" id="txtPassword" name="password-example" Placeholder = "Contraseña" required />
+            </li>
+            <li class="button">
+              <button class= "login" id="registro" type="submit">Registrarse</button>
+              <button class= "login" id="btnLogin" type="submit">Iniciar sesión</button>
+              <span id="loginGoogle" class="login"><img src="./img/googleLogo.jpg" class="icon" alt=""><p>Inicia Sesión con Google</p></span>
+              <span id="loginFacebook" class="login Facebook"><img src="" class="icon" alt=""><p>Inicia Sesión con Facebook</p></span>
+            </li>
+          </ul>
+        </form>
+      </div>`;
+  buildListenerForm();
+}
+
+/**
+ * Funcion inicia el boton de login cuando este exista
+ */
+const buildListenerForm = () => {
+
+  try {
+    //*************** */
+    // BOTON PARA LOGUEAR CON EMAIL Y PASSWORD
+    document.getElementById("form-login").addEventListener("submit", () => {
+      const email = document.getElementById("txtMail").value;
+      const password = document.getElementById("txtPassword").value;
+      emailLogin(email, password);
+    });
+
+    //******************* */
+    // BOTÓN LOGIN CON GOOGLE
+    document.getElementById("loginGoogle").addEventListener("click", () => {
+      googleLogin();
+    });
+    // BOTÓN LOGIN CON FACEBOOK
+    document.getElementById("loginFacebook").addEventListener("click", () => {
+      facebookLogin()
+    });
+      // BOTÓN QUE CREA CUENTA
+      document.getElementById("btnCreate").addEventListener("submit", () => {
+        const email = document.getElementById("txtMail").value;
+        const password = document.getElementById("txtPassword").value;
+        createAccount(email, password); 
+      });
+
+    } catch (e) {
+    console.error(e);
+    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+  }
+}
