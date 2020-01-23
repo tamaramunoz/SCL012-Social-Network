2f6fc581e671b45635bb46cd6e3e24b81e682cee
reyesvi92@gmail.com <reyesvi92@gmail.com>
2020-01-23 13:23:32 -0300

    login google

--- src/lib/google-auth.js
+++ src/lib/google-auth.js
@@ -1,7 +1,24 @@
-// aqui exportaras las funciones que necesites
 // LOGIN CON GOOGLE
-const GoogleIn = document.getElementById("loginGoogle");
+
 export const googleLogin = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
-}
+    firebase.auth().signInWithPopup(provider);
+    
+  const initFirebaseAuth = () => {
+    // Listen to auth state changes.
+    firebase.auth().onAuthStateChanged(authStateObserver);
+    console.log(initFirebaseAuth);
+  }
+  const getProfilePicUrl = () => {
+    return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
+  }
+  
+  // Returns the signed-in user's display name.
+  const getUserName = () => {
+    return firebase.auth().currentUser.displayName;
+  }
+
+  const isUserSignedIn = () => {
+    return !!firebase.auth().currentUser;
+  }
+} 
