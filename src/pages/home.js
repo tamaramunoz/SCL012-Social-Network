f15cadbdd894292b66448c69b8e267c20bbd97ef
NPGonzalez <naypili@gmail.com>
2020-01-24 20:24:14 -0300

    Depuración código archivos home y login

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,29 +6,27 @@ export const goHome = () => {
                 <a id = "btnLogOut" href="#">Cerrar sesión</a>
                 <a href="#" style="float:right">Configuración</a>
             </div>`;
-  // / BOTON LOGOUT.
-  document.getElementById('btnLogOut').addEventListener('click', () => {
-    firebase.auth().signOut()
-      .then(() => {
-        goLoginPage();
-        console.log('salir');
-      })
-      .catch((error) => {
-        console.log('error saliendo');
-      });
-  });
 };
-
-
-// #Agregando Posts
-// db.collection("posts").add({
+//   BOTON LOGOUT.
+//   document.getElementById('btnLogOut').addEventListener('click', () => {
+//     firebase.auth().signOut()
+//       .then(() => {
+//         goLoginPage();
+//         console.log('salir');
+//       })
+//       .catch((error) => {
+//         console.log('error saliendo');
+//       });
+//   });
+//   #Agregando Posts
+//   db.collection("posts").add({
 //   name: "Tami",
 //   place: "Cajon del Maipo",
 //   description: "Lugar para acampar"
-// })
-// .then(function(docRef) {
+//   })
+//   .then(function(docRef) {
 //   console.log("Document written with ID: ", docRef.id);
-// })
-// .catch(function(error) {
+//   })
+//   .catch(function(error) {
 //   console.error("Error adding document: ", error);
-// });
+//   });
