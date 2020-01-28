6935811e4105a6e2deb8e4bd870b6c62edff417d
reyesvi92@gmail.com <reyesvi92@gmail.com>
2020-01-27 23:18:09 -0300

    css page profile

--- src/lib/index.js
+++ src/lib/index.js
@@ -12,20 +12,50 @@ export const emailLogin = (email, password) => {
     console.log(error);
   });
 };
+/* Guardar datos de login en BD */
+const saveData = (userId, name, email, imageUrl) => {
+  firebase.database().ref('users/' + userId).
+  set({
+    username: name,
+    email: email,
+    picture: imageUrl,
+    id: userId,
+  });
+}
 
 // CREAR CUENTA MAIL Y PWD
 export const createAccount = (email, password) => {
   firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+    saveData(user.uid, username, user.email, picture);
+    check();
+    alert('Tu usuario ha sido registrado! \nConfirma el mensaje de verificación en tu correo y seguidamente puedes Iniciar Sesión');
+    check();
   }).catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
     if (errorCode === 'auth/email en uso') {
       alert('Correo en uso');
-    } if (errorCode === 'auth/email inválido') {
+    }
+    if (errorCode === 'auth/email inválido') {
       alert('Email inválido');
-    } if (errorCode === 'auth/password débil') {
+    }
+    if (errorCode === 'auth/password débil') {
       alert('Contraseña tiene que tener más de 8 caracteres y una mayúscula');
     }
     alert(`${errorCode}`);
   });
+
 };
+
+/* Validación de correo al usuario */
+const check = () => {
+  const user = firebase.auth().currentUser;
+  user.sendEmailVerification().then(() => {}).catch((error) => {});
+}
+
+/* Cambio de contraseña */
+const resetPassword = (email) => {
+  firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {})
+    .catch((error) => {});
+}
--- src/pages/perfil.js
+++ src/pages/perfil.js
@@ -1,21 +1,24 @@
 export const perfilInfo = () => {
 
-    const user = firebase.auth().currentUser;
-    document.getElementById("perfil-content").innerHTML =
-        `<div class="card">
-          <div class="leftcolumn">
-            <div class="card">
-              <img src= "${user.photoURL}" class="ft" alt="foto de perfil"/>
-            </div>
-          </div>
-          <div class="card"> 
-            <div class="leftcolumn">
-              <span class="name">${user.displayName}</span>
-              <span class="info">${user.email}</span>
-            </div>
-            <button  id="btnSave-perfil" type="submit">Editar perfil</button>
-            <div class="rightcolumn">
-            </div>`
+  const user = firebase.auth().currentUser;
+  document.getElementById("perfil-content").innerHTML =
+    `<div class="row">
+       <div class="side">
+         <div class="fakeimg">
+             <img src= "${user.photoURL}" class="picture" alt="foto de perfil"/>
+         </div> 
+         <div class="info"> 
+             <span class="name">${user.displayName}</span>
+             <span class="email">${user.email}</span><br>
+         </div>
+         <div class= "btnSave">
+             <button  id="btnSave-perfil" type="submit">Editar perfil</button>
+         </div>
+       </div>
+       <div class="main"> 
+       <h2>PUBLICACIONES</h2>
+       </div>
+     </div>`;
 
   /* Button editar perfil */
   document.getElementById("btnSave-perfil").addEventListener("click", () => {
@@ -23,10 +26,9 @@ export const perfilInfo = () => {
   });
 };
 
-
 const perfilEdit = () => {
   document.getElementById("perfil-content").innerHTML =
-        `<div id="modalUser" class="w3-modal"></div>
+    `<div id="modalUser" class="w3-modal"></div>
              <div class="perfil-section"
                     <div class="container-profile">
                         <div class="ft-perfil">
